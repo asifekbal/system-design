2411796b0d04aae0cf10392db41f0c86e78ec875
Asif Ekbal <asifekbal@Asifs-MacBook-Air.local>
2021-05-13 01:37:46 +0530

    first commit

---
+++ src/asif/design/LRUCache.java
@@ -0,0 +1,25 @@
+package asif.design;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LRUCache {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	private int size;
+	private Queue<String>  queue= new LinkedList<>();
+//	private Map<String,>
+	
+
+	private void put(String key, String value) {
+
+	}
+
+	private String get(String key) {
+		return null;
+	}
+
+}
